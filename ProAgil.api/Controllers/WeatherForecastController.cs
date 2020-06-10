a4f3af041b19bbbc9e46011a6201586e4c4af747
zuannysoftwares <jonatarafael_zuanny@yahoo.com>
2020-06-10 09:50:39 -0300

    Colocando no repositorio minha API desenvolvida com .NET Core

---
+++ ProAgil.api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgil.api.Data;
+using ProAgil.api.Model;
+
+namespace ProAgil.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public readonly DataContext _context;
+        public WeatherForecastController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetEventos()
+        {
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao buscar informações no banco de dados.");                
+            }
+        }
+
+        public async Task<IActionResult> GetEventoById(int id)
+        {
+            try
+            {
+                var results = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao buscar informações no banco de dados.");
+            }
+        }
+    }
+}
---
+++ ProAgil.api/Migrations/20200609172400_AdicaoImagemUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.api.Migrations
+{
+    public partial class AdicaoImagemUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Eventos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Eventos");
+        }
+    }
+}
